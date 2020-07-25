5135181205d2c008889459e7b0e7351fb580addd
zeshankarim <zeshankarim64@gmail.com>
2020-07-26 02:37:11 +0500

    complete todo app

---
+++ app.js
@@ -0,0 +1,42 @@
+var list=document.getElementById("list");
+
+function addTodo(){
+    var todo_item=document.getElementById("todo-item");
+    
+    //create li tag with text node
+    var li=document.createElement('li');
+    var liText=document.createTextNode(todo_item.value);
+    li.appendChild(liText);
+    // create delete btn
+    var deBtn=document.createElement("BUTTON");
+    var delText=document.createTextNode("DELETE");
+    deBtn.setAttribute("class","btn");
+    deBtn.setAttribute("onclick","deleteItem(this)")
+    deBtn.appendChild(delText)
+    //create edit button
+    var editBtn=document.createElement("BUTTON");
+    var editText=document.createTextNode("EDIT");
+    editBtn.appendChild(editText)
+    editBtn.setAttribute("onclick","editItem(this)")
+
+    li.appendChild(deBtn)
+    li.appendChild(editBtn)
+     
+   
+    list.appendChild(li);
+    todo_item.value=""
+    console.log(li);
+}
+function deleteItem(e){
+    e.parentNode.remove()
+
+}
+function deleteAll(){
+    list.innerHTML=""
+}
+
+function editItem(e){
+    var val=e.parentNode.firstChild.nodeValue;
+    var editValue= prompt("Enter edit value", val);
+    e.parentNode.firstChild.nodeValue=editValue
+}
