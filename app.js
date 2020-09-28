f732a1fb65b08e9dcf1cc1882d00ffa0bd871f15
zeshankarim <zeshankarim64@gmail.com>
2020-09-28 22:24:52 +0500

    finally edited

--- app.js
+++ app.js
@@ -2,11 +2,21 @@ var list=document.getElementById("list");
 
 function addTodo(){
     var todo_item=document.getElementById("todo-item");
-    
-    //create li tag with text node
-    var li=document.createElement('li');
+    if(todo_item.value==""){
+        alert("Elter avalue");
+
+    }
+    else{
+        var li=document.createElement('li');
     var liText=document.createTextNode(todo_item.value);
+    
+
+
     li.appendChild(liText);
+       
+    }
+
+ 
     // create delete btn
     var deBtn=document.createElement("BUTTON");
     var delText=document.createTextNode("DELETE");
